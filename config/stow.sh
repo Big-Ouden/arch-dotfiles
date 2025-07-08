4db007e3e56766c56d6ecac4173bcc7ec911504e
BigOuden <simon.belier29@gmail.com>
2025-07-08 10:39:57 -0400

    Update stow script

--- config/stow.sh
+++ config/stow.sh
@@ -1,1 +1,8 @@
-stow -t ~ --dotfiles .
+#!/usr/bin/env bash
+
+# Répertoire contenant les dotfiles
+DOTFILES_DIR="$HOME/arch-dotfiles/config"
+
+cd "$DOTFILES_DIR"
+
+stow -t "$HOME" --dotfiles . -v
