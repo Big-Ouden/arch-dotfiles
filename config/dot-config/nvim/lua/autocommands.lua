5852846b6d70a3294b755aa305ff76c37219496a
BigOuden <simon.belier29@gmail.com>
2025-07-14 17:25:47 -0400

    test

--- config/dot-config/nvim/lua/autocommands.lua
+++ config/dot-config/nvim/lua/autocommands.lua
@@ -1,15 +1,12 @@
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
-
 -- Highlight when yanking text
 -- See `:help vim.hl.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'highlight when yanking text',
-	group = vim.api.nvim_create_augroup('nvim-highlight-yank', {clear=true}),
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "highlight when yanking text",
+	group = vim.api.nvim_create_augroup("nvim-highlight-yank", { clear = true }),
 	callback = function()
 		vim.hl.on_yank()
 	end,
 })
-
-
